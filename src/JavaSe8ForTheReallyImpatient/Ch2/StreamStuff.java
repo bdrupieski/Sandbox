8ed52995ae678ed66789cb836cc0d08632877cdb
Brian Drupieski <brian.drupieski@gmail.com>
2016-01-04 21:27:31 -0500

    more stream stuff

--- src/JavaSe8ForTheReallyImpatient/Ch2/StreamStuff.java
+++ src/JavaSe8ForTheReallyImpatient/Ch2/StreamStuff.java
@@ -5,10 +5,8 @@ import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -27,16 +25,16 @@ public class StreamStuff {
         System.out.println(fiveRandomNumbers);
 
         System.out.println(Stream.empty().collect(Collectors.toSet()));
-        System.out.println(Stream.generate(() -> "echo").limit(5).collect(Collectors.toList()));
+        printStream(Stream.generate(() -> "echo").limit(5));
 
         Stream<BigInteger> integers = Stream.iterate(BigInteger.ZERO, n -> n.add(BigInteger.ONE));
-        System.out.println(integers.filter(x -> x.mod(BigInteger.valueOf(17)).equals(BigInteger.ZERO)).limit(5).collect(Collectors.toList()));
+        printStream(integers.filter(x -> x.mod(BigInteger.valueOf(17)).equals(BigInteger.ZERO)).limit(5));
 
         Stream<String> lowercaseWords = words.stream().map(String::toLowerCase);
-        System.out.println(lowercaseWords.limit(10).collect(Collectors.toList()));
+        printStream(lowercaseWords.limit(10));
 
         Stream<Character> characterStream = words.stream().map(StreamStuff::characterStream).flatMap(x -> x);
-        System.out.println(characterStream.limit(50).collect(Collectors.toList()));
+        printStream(characterStream.limit(50));
 
         Stream<Character> characterStream2 = words.stream().flatMap(StreamStuff::characterStream);
         printStream(characterStream2.skip(50).limit(50));
@@ -50,6 +48,39 @@ public class StreamStuff {
         printStream(longestFirst);
         Stream<String> shortestFirst = words.stream().sorted(Comparator.comparing(String::length)).limit(10);
         printStream(shortestFirst);
+
+        printStream(Stream.of("merrily", "merrily", "merrily", "gently").distinct());
+
+        Optional<String> largest = words.stream().max(String::compareToIgnoreCase);
+        System.out.println(largest);
+        Optional<String> longest = words.stream().max((x, y) -> Integer.compare(x.length(), y.length()));
+        System.out.println(longest);
+
+        String dir = Optional.ofNullable((String)null).orElseGet(() -> System.getProperty("user.dir"));
+        System.out.println(dir);
+
+        Optional<String> empty = Optional.empty();
+        System.out.println(empty.orElse("it was null"));
+
+        Optional<Double> d = Optional.of(3D);
+        Optional<Float> flatmapOptional = d.flatMap(StreamStuff::GetOptional1).flatMap(StreamStuff::GetOptional2);
+        System.out.println(flatmapOptional);
+
+        Map<String, Long> wordsToFreq = words.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        System.out.println(wordsToFreq.get("the"));
+        System.out.println(wordsToFreq.get("zigzag"));
+        Map<String, Integer> mapToFreqInt = words.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(x -> 1)));
+        System.out.println(mapToFreqInt.get("the"));
+        System.out.println(mapToFreqInt.get("zigzag"));
+
+        Map<Integer, Set<String>> wordLengthsToWordsWithThatLength = words.stream().collect(Collectors.groupingBy(String::length, Collectors.toSet()));
+        System.out.println(wordLengthsToWordsWithThatLength.get(13));
+
+        Map<Character, IntSummaryStatistics> startingCharactersToWordStats = words.stream()
+                .collect(Collectors.groupingBy(x -> x.length() > 0 ? x.charAt(0) : '~',
+                         Collectors.summarizingInt(String::length)));
+        System.out.println(startingCharactersToWordStats.get('b'));
+
     }
 
     public static <T> void printStream(Stream<T> s) {
@@ -60,5 +91,13 @@ public class StreamStuff {
         List<Character> result = new ArrayList<>();
         for (char c : s.toCharArray()) result.add(c);
         return result.stream();
+    }
+
+    public static Optional<String> GetOptional1(Double s) {
+        return Optional.of(s.toString());
+    }
+
+    public static Optional<Float> GetOptional2(String s) {
+        return Optional.of(Float.parseFloat(s));
     }
 }
